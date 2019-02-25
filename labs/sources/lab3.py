4c756669d01a81a99525f4e2a4d05f19655b12ec
Ján Magyar <>
2019-02-25 20:43:25 +0100

    updated material for lab 3

--- labs/sources/lab3.py
+++ labs/sources/lab3.py
@@ -12,14 +12,29 @@ EPISODES = 50
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
+        # number of parameters describing the state
         self.state_size = state_size
+        # number of possible actions for the agent
         self.action_size = action_size
+
+        # memory of experiences
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
+
+        # discount rate:
+        # 0 - consider only current rewards
+        # 1 - go for long-term high reward
+        self.gamma = 0.95
+
+        # learning rate:
+        # 0 - do not learn anything new
+        # 1 - forget all old information
+        self.learning_rate = 0.001
+
+        # probability of random action selection
+        self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+
         self.model = self._build_model()
 
     def _build_model(self):
@@ -37,20 +52,25 @@ class DQNAgent:
         # random action selection with probability self.epsilon
         # TODO:
 
-        # otherwise the action with highest Q value
+        # otherwise select the action with the highest Q value
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
 
     def replay(self, batch_size):
+        # select random samples from memory
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
+            # compute new target value to be the output of the network
             target = reward
             if not done:
                 target = (reward + self.gamma *
                           np.amax(self.model.predict(next_state)[0]))
+            # get current output value and update the value for the action
             target_f = self.model.predict(state)
             target_f[0][action] = target
+            # train the model
             self.model.fit(state, target_f, epochs=1, verbose=0)
+        # update epsilon to minimize exploration on the long term
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -62,30 +82,40 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
+    # load training environment and get parameters
     env = gym.make('CartPole-v1')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
+
     agent = DQNAgent(state_size, action_size)
     # agent.load("./save/cartpole-dqn.h5")
     done = False
     batch_size = 32
 
     for e in range(EPISODES):
+        # at each episode, reset environment to starting position
         state = env.reset()
         state = np.reshape(state, [1, state_size])
         for time in range(500):
+            # show game graphics
             env.render()
+
+            # select action, observe environment, calculate reward
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
             reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
+
+            # save experience and update current state
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             if done:
+                # if game ended, finish episode
                 print("episode: {}/{}, score: {}, e: {:.2}"
                       .format(e, EPISODES, time, agent.epsilon))
                 break
             if len(agent.memory) > batch_size:
+                # if enough experience is available, replay
                 agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
+
+    # agent.save("./save/cartpole-dqn.h5")
